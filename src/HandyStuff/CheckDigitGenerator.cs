469defb308769aef6cdc19152737aca5f64aadc4
steveski <steve@steve-york.com>
2025-03-21 19:15:37 +1000

    feat: implemented check digit generator

---
+++ src/HandyStuff/CheckDigitGenerator.cs
@@ -0,0 +1,119 @@
+﻿namespace HandyStuff;
+
+using System.Text;
+
+public class CheckDigitGenerator
+{
+    /// <summary>
+    /// Generate a new randomised ID with a checkdigit
+    /// </summary>
+    /// <param name="length">The total length of the random number string to be generated. The check digit will be an extra character after this</param>
+    /// <returns>Random sequence of numbers including a checkdigit appended</returns>
+    public string NewRandomNumberSequenceWithCheckDigit(int length = 8, int[]? weightingSequence = null)
+    {
+        var numberString = GenerateRandomNumericString(length - 1);
+        var checkDigit = GenerateCheckDigit(numberString, weightingSequence);
+
+        return numberString + checkDigit;
+    }
+
+    /// <summary>
+    /// Generate a check digit for an provided input string
+    /// </summary>
+    /// <param name="input">Any string that you would like to generate a digit for</param>
+    /// <param name="weightingSequence">A sequence of characters that will be used for the weighting of the each character in the input string</param>
+    /// <returns>The check digit for the provided input</returns>
+    public char GenerateCheckDigit(string input, int[]? weightingSequence = null)
+    {
+        if (weightingSequence == null)
+        {
+            SetDefaultWeightingSequence(ref weightingSequence, input);
+        }
+
+        int sum = 0;
+        int weightingSequenceIndex = 0;
+
+        for (int index = 0; index < input.Length; index++)
+        {
+            sum += input[index] * weightingSequence![weightingSequenceIndex];
+            weightingSequenceIndex = NextWeightSequenceIndex(weightingSequenceIndex, weightingSequence!);
+        }
+
+        // Map sum to an ascii letter
+        return (char)('A' + sum % 26);
+    }
+
+    private void SetDefaultWeightingSequence(ref int[]? weightingSequence, string input)
+    {
+        int length = input.Length;
+        weightingSequence = new int[length];
+        int left = 0, right = length - 1;
+        int value = length;
+
+        while (left <= right)
+        {
+            if (left == right)
+            {
+                weightingSequence[left] = value--;
+            }
+            else
+            {
+                weightingSequence[left] = value--;
+                weightingSequence[right] = value--;
+            }
+
+            left++;
+            right--;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="currentIndex"></param>
+    /// <param name="sequence"></param>
+    /// <returns>The weightingSequence index</returns>
+    private int NextWeightSequenceIndex(int currentIndex, int[] sequence)
+    {
+        if (currentIndex == sequence.Length - 1)
+        {
+            return 0;
+        }
+
+        currentIndex++;
+
+        return currentIndex;
+    }
+
+    /// <summary>
+    /// Generate a random sequence numbers returning them as a string
+    /// </summary>
+    /// <param name="length">How many digits to generate</param>
+    /// <returns>The randomised string of the length specified</returns>
+    private string GenerateRandomNumericString(int length)
+    {
+        var random = new Random();
+        var number = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++)
+        {
+            number.Append(random.Next(0, 9)).ToString();
+        }
+
+        return number.ToString();
+    }
+
+    /// <summary>
+    /// Attempt to validate the check digit of the specified input string
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public bool TryIsCheckDigitValid(string input)
+    {
+        var theString = input[..^1];
+        var checkDigit = GenerateCheckDigit(theString);
+
+        return input[^1] == checkDigit;
+    }
+
+}
---
+++ src/Tests/HandyStuff.UnitTests/CheckDigitGeneratorTests.cs
@@ -0,0 +1,140 @@
+﻿namespace HandyStuff.UnitTests;
+
+using Shouldly;
+
+public class CheckDigitGeneratorTests
+{
+    [Fact]
+    public void GenerateCheckDigit_For3DigitInput_GeneratesValidCheckDigit_OddLength()
+    {
+        var sut = new CheckDigitGenerator();
+
+        var checkDigit = sut.GenerateCheckDigit("123");
+
+        // Expected weights:
+        // Digit 1: 3
+        // Digit 2: 1
+        // Digit 3: 2
+        //
+        // Therefore ( ( '1' * 3) + ('2' * 1) + ('3' * 2) ) % 26 + 'A'
+        //    Values ( ( 49 * 3) +  (50 * 1) +  (51 * 2) )  % 26 + 65
+
+        var expected = (char)((('1' * 3) + ('2' * 1) + ('3' * 2)) % 26 + 'A');
+        checkDigit.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void GenerateCheckDigit_For4DigitInput_GeneratesValidCheckDigit_EvenLength()
+    {
+        var sut = new CheckDigitGenerator();
+
+        var checkDigit = sut.GenerateCheckDigit("1234");
+
+        // Expected weights:
+        // Digit 1: 4
+        // Digit 2: 2
+        // Digit 3: 1
+        // Digit 4: 3
+        //
+        // Therefore ( ( '1' * 4) + ('2' * 2) + ('3' * 1) + ('4' * 3 ) % 26 + 'A'
+        //    Values ( ( 49 * 4) +  (50 * 2) +  (51 * 1) +  (52  * 3) )  % 26 + 65
+
+        var expected = (char)((('1' * 4) + ('2' * 2) + ('3' * 1) + ('4' * 3)) % 26 + 'A');
+        checkDigit.ShouldBe(expected);
+    }
+
+
+    [Fact]
+    public void GenerateCheckDigit_For6CharacterInput_GeneratesValidCheckDigit_EvenLength()
+    {
+        var sut = new CheckDigitGenerator();
+
+        var checkDigit = sut.GenerateCheckDigit("ABC321");
+
+        // Expected weights:
+        // Digit 1: A
+        // Digit 2: B
+        // Digit 3: C
+        // Digit 4: 3
+        // Digit 5: 2
+        // Digit 6: 1
+        //
+        // Therefore ( ( 'A' * 6 ) + ( 'B' * 4 ) + ( 'C' * 2 ) + ( '3' * 1 ) + ( '2' * 3 ) + ( '1' * 5 ) % 26 + 'A'
+        //    Values ( ( 65 * 6 )  + ( 65 * 4 )  + ( 66 * 2 )  + ( 51  * 1 ) + ( 50  * 3 ) + ( 49  * 5 ) )  % 26 + 65
+
+        var expected = (char)((('A' * 6) + ('B' * 4) + ('C' * 2) + ('3' * 1) + ('2' * 3) + ('1' * 5)) % 26 + 'A');
+        checkDigit.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void GenerateCheckDigitWithWeightingSequenceProvided()
+    {
+        var sut = new CheckDigitGenerator();
+        int[] weightingSequence = [7, 5, 3, 1, 2, 4, 6];
+
+        var checkDigit = sut.GenerateCheckDigit("ABC4321", weightingSequence);
+
+        var expected = (char)((
+            ('A' * weightingSequence[0]) +
+            ('B' * weightingSequence[1]) +
+            ('C' * weightingSequence[2]) +
+            ('4' * weightingSequence[3]) +
+            ('3' * weightingSequence[4]) +
+            ('2' * weightingSequence[5]) +
+            ('1' * weightingSequence[6]))
+            % 26 + 'A');
+        checkDigit.ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData("ABC4321", "7,5,3,1,2,4,6", 'W')]
+    [InlineData("ABC7654321", "4,2,1,3", 'T')]
+    [InlineData("ABC4321", "10,8,6,4,2,1,3,5,7,9", 'H')]
+    public void GenerateCheckDigitWithVaryingWeightingSequences(string input, string weightingSequenceString, char expected)
+    {
+        var sut = new CheckDigitGenerator();
+        int[] weightingSequence = weightingSequenceString
+            .Split(',')
+            .Select(int.Parse)
+            .ToArray();
+
+        var checkDigit = sut.GenerateCheckDigit(input, weightingSequence);
+
+        checkDigit.ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData("ABC4321", 'W')]
+    [InlineData("0192837465", 'X')]
+    [InlineData("ABC123DEF456", 'E')]
+    public void GenerateCheckDigitWithNoWeightinSequenceProvided(string input, char expected)
+    {
+        var sut = new CheckDigitGenerator();
+
+        var checkDigit = sut.GenerateCheckDigit(input);
+
+        checkDigit.ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData("ABC4321W", true)]
+    [InlineData("0192837465X", true)]
+    [InlineData("ABC123DEF456E", true)]
+    [InlineData("ASDASDT", false)]
+    public void TryIsCheckDigitValidReturnsExpected(string inputWithCheckDigit, bool expected)
+    {
+        var sut = new CheckDigitGenerator();
+        var isvalidCheckDigit = sut.TryIsCheckDigitValid(inputWithCheckDigit);
+        isvalidCheckDigit.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void NewRandomNumberSequenceWithCheckDigitReturnsIdWithCorrectCheckDigit()
+    {
+        var sut = new CheckDigitGenerator();
+        var newId = sut.NewRandomNumberSequenceWithCheckDigit();
+        var isvalidCheckDigit = sut.TryIsCheckDigitValid(newId);
+        isvalidCheckDigit.ShouldBeTrue();
+    }
+
+}
